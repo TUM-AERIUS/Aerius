2217a11c2926e8551ec14af8578967cb4d330de8
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2017-03-17 19:23:49 +0100

    hack direction

--- Network/playing.py
+++ Network/playing.py
@@ -4,6 +4,7 @@ Once a model is learned, use this to play it.
 
 import carmunk
 import numpy as np
+import math
 from nn import neural_net
 import tensorflow as tf
 
@@ -24,13 +25,31 @@ def play(session, state, prediction):
     # Move.
     while True:
         car_distance += 1
+        obstacle = False
+        for i in range(40):
+            if gameState[0][i] < 10:
+                obstacle = True
+                break
 
-        feed_dict = {
-            state: gameState
-        }
+        if obstacle:
+            feed_dict = {
+                state: gameState
+            }
 
-        # Choose action.
-        action = np.argmax(session.run([prediction], feed_dict=feed_dict))
+            # Choose action.
+            action = np.argmax(session.run([prediction], feed_dict=feed_dict))
+
+        else:
+            cos_angle = math.cos(gameState[0][41] - gameState[0][44])
+            sin_angle = math.sin(gameState[0][41] - gameState[0][44])
+
+            if cos_angle < 0.92:
+                if sin_angle > 0:
+                    action = 0
+                else:
+                    action = 1
+            else:
+                action = 2
 
         # Take action.
         _, gameState = game_state.frame_step(action)
