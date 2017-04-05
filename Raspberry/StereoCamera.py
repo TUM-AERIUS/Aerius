f2d625dfeac3932b11186b0501742ddf0fdde68b
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2017-04-06 02:43:06 +0300

    made camera calibration a separate file

--- Raspberry/StereoCamera.py
+++ Raspberry/StereoCamera.py
@@ -1,12 +1,24 @@
+from matplotlib import pyplot as plt
 import numpy as np
 import cv2
 import socket
 import struct
 import io
-from matplotlib import pyplot as plt
+import json
+import os
+import sys
 
+# Useful references:
+# http://docs.opencv.org/3.0-beta/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html#reprojectimageto3d
+# use StereoMatcher::compute to get the disparity matrix
+# get the the points in 3d space, needs disparity and other values we got from calibration
+
+cameraDataFolder = "cameraData.json"
+path_to_script = os.path.realpath(__file__)
+parent_directory = os.path.dirname(path_to_script)
 
 def getStereoImages(connection):
+    # Requesta new photo from the photo server
     connection.write(struct.pack('<L', 1))
     connection.flush()
 
@@ -44,126 +56,32 @@ photoSocket.listen(0)
 
 photoConnection = photoSocket.accept()[0].makefile("rwb")
 
-numBoards = 30  # how many boards would you like to find
-board_w = 7
-board_h = 6
+# Get calibration data from camerData.json
+if os.path.isfile(os.path.join(parent_directory, cameraDataFolder)):
+    cameraData = json.loads(cameraDataFolder)
+else:
+    print("No calibration data found")
+    sys.exit(0)
 
-board_sz = (7, 6)
-board_n = board_w*board_h
+Q = cameraData["Q"]
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+window_size = 3
+min_disp = 16
+num_disp = 112-min_disp
+stereo = cv2.StereoSGBM_create(minDisparity = min_disp,
+    numDisparities = num_disp,
+    blockSize = 16,
+    P1 = 8*3*window_size**2,
+    P2 = 32*3*window_size**2,
+    disp12MaxDiff = 1,
+    uniquenessRatio = 10,
+    speckleWindowSize = 100,
+    speckleRange = 32
+)
 
-# Arrays to store object points and image points from all the images.
-object_points = []  # 3d point in real world space
-imagePoints1 = []  # 2d points in image plane.
-imagePoints2 = []  # 2d points in image plane.
-
-corners1 = []
-corners2 = []
-
-obj = np.zeros((6*7, 3), np.float32)
-obj[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
-
-leftImage, rightImage = getStereoImages(photoConnection)
-
-# vidStreamL = cv2.VideoCapture(0)  # index of your camera
-# vidStreamR = cv2.VideoCapture(1)  # index of your camera
-success = 0
-k = 0
-found1 = False
-found2 = False
-
-while success < numBoards:
-
+while True:
     img1, img2 = getStereoImages(photoConnection)
-    # retL, img1 = vidStreamL.read()
-    height, width, depth = img1.shape
-    # retR, img2 = vidStreamR.read()
-    #resize(img1, img1, Size(320, 280));
-    #resize(img2, img2, Size(320, 280));
-    gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-
-    found1, corners1 = cv2.findChessboardCorners(img1, board_sz)
-    found2, corners2 = cv2.findChessboardCorners(img2, board_sz)
-
-    if found1:
-        cv2.cornerSubPix(gray1, corners1, (11, 11), (-1, -1),criteria)
-        cv2.drawChessboardCorners(gray1, board_sz, corners1, found1)
-
-    if found2:
-        cv2.cornerSubPix(gray2, corners2, (11, 11), (-1, -1), criteria)
-        cv2.drawChessboardCorners(gray2, board_sz, corners2, found2)
-
-    cv2.imshow('image1', gray1)
-    cv2.imshow('image2', gray2)
-
-    k = cv2.waitKey(100)
-    print(k)
-
-    if k == 27:
-        break
-    if k == 32 and found1 != 0 and found2 != 0:
-
-        imagePoints1.append(corners1)
-        imagePoints2.append(corners2)
-        object_points.append(obj)
-        print("Corners stored\n")
-        success += 1
-
-        if success >= numBoards:
-            break
-
-cv2.destroyAllWindows()
-print("Starting Calibration\n")
-cameraMatrix1 = cv2.cv.CreateMat(3, 3, cv2.CV_64FC1)
-cameraMatrix2 = cv2.cv.CreateMat(3, 3, cv2.CV_64FC1)
-
-retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(object_points, imagePoints1, imagePoints2, (width, height))
-
-print("Done Calibration\n")
-print("Starting Rectification\n")
-R1 = np.zeros(shape=(3,3))
-R2 = np.zeros(shape=(3,3))
-P1 = np.zeros(shape=(3,3))
-P2 = np.zeros(shape=(3,3))
-
-cv2.stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2,(width, height), R, T, R1, R2, P1, P2, Q=None, flags=cv2.cv.CV_CALIB_ZERO_DISPARITY, alpha=-1, newImageSize=(0,0))
-
-print("Done Rectification\n")
-print("Applying Undistort\n")
-
-map1x, map1y = cv2.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, P1, (width, height), cv2.CV_32FC1)
-map2x, map2y = cv2.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R2, P2, (width, height), cv2.CV_32FC1)
-
-print("Undistort complete\n")
-
-# Testing, delete if correct
-# while True:
-#     img1, img2 = getStereoImages(photoConnection)
-#     imgU1 = np.zeros((height, width, 3), np.uint8)
-#     imgU1 = cv2.remap(img1, map1x, map1y, cv2.INTER_LINEAR, imgU1, cv2.BORDER_CONSTANT, 0)
-#     imgU2 = cv2.remap(img2, map2x, map2y, cv2.INTER_LINEAR)
-#     cv2.imshow("imageL", img1)
-#     cv2.imshow("imageR", img2)
-#     cv2.imshow("image1L", imgU1)
-#     cv2.imshow("image2R", imgU2)
-#     k = cv2.waitKey(5)
-#     if k == 27:
-#         break
-# End of test
-
-# http://docs.opencv.org/3.0-beta/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html#reprojectimageto3d
-# use StereoMatcher::compute to get the disparity matrix
-img1, img2 = getStereoImages(photoConnection)
-imgU1 = np.zeros((height, width, 3), np.uint8)
-imgU1 = np.zeros((height, width, 3), np.uint8)
-imgU1 = cv2.remap(img1, map1x, map1y, cv2.INTER_LINEAR, imgU1, cv2.BORDER_CONSTANT, 0)
-imgU2 = cv2.remap(img2, map2x, map2y, cv2.INTER_LINEAR)
-disparity = cv2.StereoBM.compute(imgU1, imgU2)
-# Disparity picture for testing
-plt.imshow(disparity, 'gray')
-
-# get the the points in 3d space, needs disparity and other values we got from calibration
-image3D = cv2.reprojectImageTo3D(disparity, Q)
+    disparity = stereo.compute(img1, img2)
+    image3D = cv2.reprojectImageTo3D(disparity, Q)
+    plt.imshow(disparity, 'gray')
+    plt.show()
---
+++ Raspberry/StereoCameraCalibration.py
@@ -0,0 +1,113 @@
+import cv2
+import numpy as np
+import json
+
+imagesFolder = "stereo-photos/"
+cameraData = "cameraData.json"
+
+numBoards = 30  # how many boards would you like to find
+board_w = 3
+board_h = 3
+
+board_sz = (9, 6)
+board_n = board_w*board_h
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# Arrays to store object points and image points from all the images.
+object_points = []  # 3d point in real world space
+imagePoints1 = []  # 2d points in image plane.
+imagePoints2 = []  # 2d points in image plane.
+
+corners1 = []
+corners2 = []
+
+obj = np.zeros((9*6, 3), np.float32)
+obj[:, :2] = np.mgrid[0:6, 0:9].T.reshape(-1, 2)
+
+success = 0
+k = 0
+i = 1
+found1 = False
+found2 = False
+
+while success < numBoards and i < 10:
+
+    img1 = cv2.imread(imagesFolder + "left" + str(i) + ".jpg", 1)
+    img2 = cv2.imread(imagesFolder + "right" + str(i) + ".jpg", 1)
+
+    img1 = cv2.resize(img1, (320, 280))
+    img2 = cv2.resize(img1, (320, 280))
+
+    height, width, depth = img1.shape
+
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    found1, corners1 = cv2.findChessboardCorners(img1, board_sz)
+    found2, corners2 = cv2.findChessboardCorners(img2, board_sz)
+
+    if found1:
+        cv2.cornerSubPix(gray1, corners1, (11, 11), (-1, -1),criteria)
+        cv2.drawChessboardCorners(gray1, board_sz, corners1, found1)
+
+    if found2:
+        cv2.cornerSubPix(gray2, corners2, (11, 11), (-1, -1), criteria)
+        cv2.drawChessboardCorners(gray2, board_sz, corners2, found2)
+
+    if found1 and found2:
+        imagePoints1.append(corners1)
+        imagePoints2.append(corners2)
+        # Not sure where obj comes from..
+        object_points.append(obj)
+        print("Corners stored")
+        success += 1
+
+    if success >= numBoards:
+        break
+
+    i += 1
+
+print("Starting Calibration\n")
+
+cameraMatrix1 = np.array((3, 4, cv2.CV_64FC1), np.uint8)
+cameraMatrix2 = np.array((3, 4, cv2.CV_64FC1), np.uint8)
+
+distCoeffs1 = np.array((8, 1), np.uint8)
+distCoeffs2 = np.array((8, 1), np.uint8)
+
+retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F \
+      = cv2.stereoCalibrate(object_points, imagePoints1, imagePoints2,
+                            cameraMatrix1, distCoeffs1, cameraMatrix2,
+                            distCoeffs2, (width, height))
+
+print("Done Calibration\n")
+print("Starting Rectification\n")
+R1 = np.zeros(shape=(3, 3))
+R2 = np.zeros(shape=(3, 3))
+P1 = np.zeros(shape=(3, 3))
+P2 = np.zeros(shape=(3, 3))
+Q = np.zeros(shape=(4, 4))
+
+cv2.stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2,
+                  distCoeffs2,(width, height), R, T, R1, R2,
+                  P1, P2, Q, flags=cv2.CALIB_ZERO_DISPARITY,
+                  alpha=-1, newImageSize=(0,0))
+
+print("Done Rectification\n")
+print("Applying Undistort\n")
+
+map1x, map1y = cv2.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, P1, (width, height), cv2.CV_32FC1)
+map2x, map2y = cv2.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R2, P2, (width, height), cv2.CV_32FC1)
+
+print("Undistort complete\n")
+
+data = {"cameraMatrixLeft"  : cameraMatrix1,
+        "cameraMatrixRight" : cameraMatrix2,
+        "distCoeffsLeft"    : distCoeffs1,
+        "distCoeffsRight"   : distCoeffs2,
+        "Q"                 : Q}
+
+with open(cameraData, "w") as f:
+    json.dump(data, f)
